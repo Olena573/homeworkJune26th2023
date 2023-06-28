7a061160c3da95ba15b082676dd1f8385ff92601
Olena573 <linboykomail@gmail.com>
2023-06-28 20:09:09 +0300

    This is a finished homework, with 3 more simple tasks.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
@@ -26,6 +28,14 @@ public class Main {
         //Here we used some methods of the class B.
 
 
+        //Here are some actions with the actions from task 3.
+        System.out.println(ClassForTaskThree.Calc.ADD.action(114, 12));
+        System.out.println(ClassForTaskThree.Calc.SUBSTRACT.action(56, 22));
+        System.out.println(ClassForTaskThree.Calc.MULTIPLY.action(2, 24));
+        System.out.println(ClassForTaskThree.Calc.DIVIDE.action(44, 11));
+        System.out.println(ClassForTaskThree.Calc.EXPONENTIATION.action(5, 2));
+
+        //Actions with enum from task 2 is in the file of the "ClassForTaskTwo" class
 
 
     }
